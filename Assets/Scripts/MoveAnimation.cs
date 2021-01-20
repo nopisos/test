df93d0669dc5bf20b6194740787109ccec16473c
nopisos <77279082+nopisos@users.noreply.github.com>
2021-01-20 13:04:23 +0300

    k

---
+++ Assets/Scripts/MoveAnimation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveAnimation : MonoBehaviour
+{
+    private Animator _animator;
+
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {        
+        if (Input.GetKey(KeyCode.D))
+            _animator.SetBool("runRight", true);
+        else
+            _animator.SetBool("runRight", false);
+        if (Input.GetKey(KeyCode.A))
+            _animator.SetBool("runLeft", true);
+        else
+            _animator.SetBool("runLeft", false);  
+    }
+}
